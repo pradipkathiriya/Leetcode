6c6e5e699073d3e0c80b26cc4151347cc993fdf7
pradipkathiriya <pradip@terpmail.umd.edu>
2024-01-21 00:28:19 -0500

    232 Valid Anagram

---
+++ Python/242 Valid Anagram.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        # if len(s) ==  len(t) == 0:
+        #     return True
+
+        count_s , count_t = {}, {}
+
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+
+        return count_s == count_t
+
+        
+        
